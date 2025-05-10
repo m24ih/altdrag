5f899cf10771c40f163c8d2d416ff855bdb2a56c
Melih Ak <melihak635442@gmail.com>
2025-05-10 14:28:40 +0300

    all done

--- localization/tr_TR/strings.h
+++ localization/tr_TR/strings.h
@@ -1,5 +1,5 @@
 ﻿/*
-  AltDrag - en-US localization by Stefan Sundin
+  AltDrag - tr-TR localization by Melih Ak
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
